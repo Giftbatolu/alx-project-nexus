d7eca7cfd7eee6fd221bd745e81ac23ba6d0219a
giftbatolu <sekinatoyelami@gmail.com>
2025-11-30 08:38:55 +0100

    feat: Add model to admin interface

--- social_app/user/admin.py
+++ social_app/user/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from posts.models import Post
+from comments.models import Comment
+from follow.models import Follow
+from interactions.models import Like, Repost
 
-# Register your models here.
+User = get_user_model()
+
+# @admin.register(CustomUser)
+# class CustomUserAdmin(admin.ModelAdmin):
+#     list_display = ("username", "email", "is_staff", "is_active")
+#     search_fields = ("username", "email")
+#     list_filter = ("is_staff", "is_active")
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Follow)
+admin.site.register(Like)
+admin.site.register(Repost)
